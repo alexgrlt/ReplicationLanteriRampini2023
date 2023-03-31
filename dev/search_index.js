c4852954233206f415ddabc557ac59ddf360520a
Documenter.jl <documenter@juliadocs.github.io>
2023-03-31 19:00:54 +0000

    build based on b200953

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReplicationLanteriRampini2023","category":"page"},{"location":"#ReplicationLanteriRampini2023","page":"Home","title":"ReplicationLanteriRampini2023","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReplicationLanteriRampini2023.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReplicationLanteriRampini2023]","category":"page"}]
+}
