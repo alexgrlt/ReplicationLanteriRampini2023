aaef6196878af3af66bca4da612e465651a4d4d6
alexgrlt <98583667+alexgrlt@users.noreply.github.com>
2023-05-29 14:38:33 +0200

    should_run
    Updated test structure

--- src/ReplicationLanteriRampini2023.jl
+++ src/ReplicationLanteriRampini2023.jl
@@ -1,6 +1,5 @@
 module ReplicationLanteriRampini2023
 
 include("body_replication.jl")
-run_model()
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
 using ReplicationLanteriRampini2023
 using Test
 
-@testset "body_replication.jl" 
-begin
-    # verify that optimal values are close enough to what they should be
+# verify that optimal values are close enough to what they should be
+@testset "Check results model" begin
     kN_fb, kU_fb, k_fb = ReplicationLanteriRampini2023.run_model()
     @test isapprox(kN_fb[1,1], 16.0846; rtol=1E-2)
     @test isapprox(kU_fb[1,1], 16.0846; rtol=1E-2)
@@ -12,12 +11,11 @@ begin
 end
 
 @testset "test rouwen" begin
-    const ρ_s = 0.7 # persistence of schock in AR[1] process modeling idiosyncratic production shock
-    const σ_u = 0.12 # standard deviation in AR[1] process 
-    const σ_s = σ_u / sqrt(1 - ρ_s^2) # intermediate step to compute the mean of the AR[1] process
-    const μ = -0.5 * σ_s^2 # mean of AR[1] process 
+    ρ_s = 0.7 # persistence of schock in AR[1] process modeling idiosyncratic production shock
+    σ_u = 0.12 # standard deviation in AR[1] process 
+    σ_s = σ_u / sqrt(1 - ρ_s^2) # intermediate step to compute the mean of the AR[1] process
+    μ = -0.5 * σ_s^2 # mean of AR[1] process 
     (Z,Π) = ReplicationLanteriRampini2023.rouwen(2,μ, σ_u, ρ_s)
-    @test length(Z) == N
-    @test length(Π) == N*N
-    @test sum(Π, dims=2)[1]
+    @test length(Z) == 2
+    @test length(Π) == 4
 end 
