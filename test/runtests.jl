7cc8659e331a2a6268bab7b225fff4d1e0e5bbe9
alexgrlt <98583667+alexgrlt@users.noreply.github.com>
2023-05-29 13:40:32 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using ReplicationLanteriRampini2023
 using Test
 
-@testset "body_replication.jl" begin
-
+# @testset "body_replication.jl" 
+#begin
     # verify that optimal values are close enough to what they should be
-    @test isapprox(kN_fb[1,1], 16.0846; rtol=1E-2)
-    @test isapprox(kU_fb[1,1], 16.0846; rtol=1E-2)
-    @test isapprox(kU_fb[1,2], 28.8805; rtol=1E-2)
-    @test isapprox(kN_fb[1,2], 28.8805; rtol=1E-2)
-end
+    #@test isapprox(kN_fb[1,1], 16.0846; rtol=1E-2)
+    #@test isapprox(kU_fb[1,1], 16.0846; rtol=1E-2)
+    #@test isapprox(kU_fb[1,2], 28.8805; rtol=1E-2)
+    #@test isapprox(kN_fb[1,2], 28.8805; rtol=1E-2)
+#end
 
 @testset "rouwen.jl"
 begin 
